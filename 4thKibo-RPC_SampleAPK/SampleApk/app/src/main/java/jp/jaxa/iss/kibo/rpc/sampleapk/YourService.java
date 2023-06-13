8f057274a80893b7cf514f1afb275085b9bbbf8c
Azfar Shams <azfarshams1998@gmail.com>
2023-06-13 15:12:48 +0600

    Update YourService.java

--- 4thKibo-RPC_SampleAPK/SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ 4thKibo-RPC_SampleAPK/SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -8,9 +8,17 @@ import gov.nasa.arc.astrobee.android.gs.MessageType;
 import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
 
-import gov.nasa.arc.ff.ocu.commands.ScanQrCodeCommand;
-import gov.nasa.arc.ff.ocu.commands.ScanQrCodeResponse;
-import gov.nasa.arc.ff.ocu.commands.Status;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.ReaderException;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 import org.opencv.core.Mat;
 
